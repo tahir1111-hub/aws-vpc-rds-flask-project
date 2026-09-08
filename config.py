4ce9523e25cb22a2125f150e6875c24aa22603cf
Tahir <tahirchhipa1111>
2026-09-08 09:41:44 +0000

    Initial commit

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ Database configuration settings
 
 # MySQL Configuration
 MYSQL_CONFIG = {
-    'host': 'localhost',
-    'user': 'flaskuser',  # Example username
-    'password': 'your_password_here',  # Replace with your actual password in production
+    'host': 'database-1.cqf2cswq29q9.us-east-1.rds.amazonaws.com',
+    'user': 'Endproject',  # Example username
+    'password': 'endproject12345',  # Replace with your actual password in production
     'port': 3306
 }
